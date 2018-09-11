b37cb8b6091024534ddeebcb77be0689ede0180b
Doan Van Quang <quang99dev@gmail.com>
2018-09-11 21:07:29 +0700

    Update

--- src/week1/Student.java
+++ src/week1/Student.java
@@ -1,38 +1,60 @@
 package week1;
 
 public class Student {
+    private String name, id, group, email;
 
-    // TODO: khai báo các thuộc tính cho Student
-
-    // TODO: khai báo các phương thức getter, setter cho Student
-
-    /**
-     * Constructor 1
-     */
-    Student() {
-        // TODO:
+    public Student(String name, String id, String group, String email) {
+        this.name = name;
+        this.id = id;
+        this.group = group;
+        this.email = email;
     }
 
-    /**
-     * Constructor 2
-     * @param n
-     * @param sid
-     * @param em
-     */
-    Student(String n, String sid, String em) {
-        // TODO:
+    public Student(String name, String id, String email) {
+        this(name, id, "INT22041", email);
     }
 
-    /**
-     * Constructor 3
-     * @param s
-     */
-    Student(Student s) {
-        // TODO:
+    public Student() {
+        this("Student", "000", "INT22041", "uet@vnu.edu.vn");
     }
 
-    String getInfo() {
-        // TODO:
-        return null; // xóa dòng này sau khi cài đặt
+    public Student(Student student) {
+        this(student.name, student.id, student.group, student.email);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void getInfo() {
+        System.out.println(this.name + " " + this.id + " " + this.group + " " + this.email);
     }
 }
--- src/week1/StudentManagement.java
+++ src/week1/StudentManagement.java
@@ -1,23 +1,53 @@
 package week1;
 
 public class StudentManagement {
+    private Student[] students = new Student[100];
+    private int index = 0;
 
-    // TODO: khai báo thuộc tính students là array chứa các đối tượng thuộc lớp Student (max. 100)
-
-    public boolean sameGroup(Student s1, Student s2) {
-        // TODO:
-        return false; // xóa dòng này sau khi cài đặt
+    public static boolean sameGroup(Student s1, Student s2) {
+        return s1.getGroup().equals(s2.getGroup());
     }
 
-    void studentsByGroup() {
+    public void addStudent(Student student) {
+        students[index++] = student;
+    }
+
+    public void printStudent() {
+        for (int i = 0; i < index; i++) {
+            students[i].getInfo();
+        }
+    }
+
+    public void studentsByGroup() {
         // TODO:
     }
 
-    void removeStudent(String id) {
-        // TODO:
+    public void removeStudent(String id) {
+        for (int i = 0; i < index; i++) {
+            if (students[i].getId().equals(id)) {
+                if (i == index - 1) students[i] = null;
+                while (i < index - 1) {
+                    students[i] = students[i + 1];
+                    i++;
+                }
+                students[i + 1] = null;
+                index--;
+                break;
+            }
+        }
     }
 
     public static void main(String[] args) {
-        // TODO:
+        StudentManagement sm = new StudentManagement();
+        Student s = new Student("name", "id", "group", "email");
+        sm.addStudent(s);
+        s = new Student(s);
+        s.setId("ksdhfkjs");
+        sm.addStudent(s);
+        s = new Student(s);
+        s.setId("HHH");
+        sm.addStudent(s);
+        sm.removeStudent("HHH");
+        sm.printStudent();
     }
 }
