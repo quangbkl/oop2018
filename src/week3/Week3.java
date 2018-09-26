bd50a9a0651a77b7c658b1f7acbc59c1cac28525
Doan Van Quang <quang99dev@gmail.com>
2018-09-26 18:01:33 +0700

    First week 3

--- src/week3/Week3.java
+++ src/week3/Week3.java
@@ -3,23 +3,29 @@ package week3;
 public class Week3 {
 
     public static int max(int m, int n) {
-        // TODO: Tìm giá trị lớn nhất của hai số nguyên, giá trị trả về của hàm là số lớn nhất
-        return -1;
+        // Tìm giá trị lớn nhất của hai số nguyên, giá trị trả về của hàm là số lớn nhất
+        if (m > n) return m;
+        return n;
     }
 
     public static int minOfArray(int[] array) {
-        // TODO: Tìm giá trị nhỏ nhất của của một mảng số nguyên (kích thước mảng <= 100 phần tử)
-        return -1;
+        // Tìm giá trị nhỏ nhất của của một mảng số nguyên (kích thước mảng <= 100 phần tử)
+        int reslut = array[0];
+        for (int i = 0, length = array.length; i < length; i++) {
+            if (reslut < array[i]) reslut = array[i];
+        }
+
+        return reslut;
     }
 
-    /**
-     * Chương trình tính chỉ số BMI và in ra kết quả đánh giá
-     * @param weight cân nặng
-     * @param height chiều cao
-     * @return Thiếu cân, Bình thường, Thừa cân, Béo phì
-     */
     public static String calculateBMI(double weight, double height) {
-        // TODO: Viết chương trình tính chỉ số BMI và in ra kết quả đánh giá
-        return null;
+        // Viết chương trình tính chỉ số BMI và in ra kết quả đánh giá
+        double BMI = weight / Math.pow(height, 2);
+
+        if (BMI < 18.5) return "Thiếu cân";
+        if (BMI < 23) return "Bình thường";
+        if (BMI < 25) return "Thừa cân";
+
+        return "Béo phì";
     }
 }
