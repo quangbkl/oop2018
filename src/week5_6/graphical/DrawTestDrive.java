56d2c6d75b1ba1ca884deecf44687fa9635c310b
Doan Van Quang <quang99dev@gmail.com>
2018-10-16 15:11:00 +0700

    draw triangle

--- src/week5_6/graphical/DrawTestDrive.java
+++ src/week5_6/graphical/DrawTestDrive.java
@@ -1,9 +1,6 @@
 package week5_6.graphical;
 
-import week5_6.Circle;
-import week5_6.Point;
-import week5_6.Rectangle;
-import week5_6.Shape;
+import week5_6.*;
 
 import java.util.ArrayList;
 
@@ -13,9 +10,11 @@ public class DrawTestDrive {
         Circle circle = new Circle(40, new Point(40, 40));
         circle.setColor("green");
         Rectangle rectangle = new Rectangle(new Point(100, 100), 40, 60);
+        Triangle triangle = new Triangle(new Point(0, 0), new Point(40, 30), new Point(30, 20));
         ArrayList<Shape> shapes = new ArrayList<>();
         shapes.add(circle);
         shapes.add(rectangle);
+        shapes.add(triangle);
         drawShape.drawShapes(shapes);
     }
 }
--- src/week5_6/graphical/GroundDraw.java
+++ src/week5_6/graphical/GroundDraw.java
@@ -1,6 +1,6 @@
 package week5_6.graphical;
 
-import week5_6.Circle;
+import week5_6.*;
 import week5_6.Point;
 import week5_6.Rectangle;
 import week5_6.Shape;
@@ -57,6 +57,22 @@ public class GroundDraw extends JPanel {
         }
     }
 
+    public void paintTriangle(Triangle triangle, Graphics g) {
+        Point point1 = triangle.getPoint1();
+        Point point2 = triangle.getPoint2();
+        Point point3 = triangle.getPoint3();
+        int x[] = {0, 10, 0};
+        int y[] = {0, 0, 10};
+
+        Color color = ColorManager.getColor(triangle.getColor());
+        g.setColor(color);
+        if (triangle.isFilled()) {
+            g.fillPolygon(x, y, 3);
+        } else {
+//            g.drawRect(x, y, width, height);
+        }
+    }
+
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         for (Shape shape : shapes) {
@@ -68,7 +84,10 @@ public class GroundDraw extends JPanel {
                 Rectangle rectangle = (Rectangle) shape;
                 this.paintRectangle(rectangle, g);
             }
-
+            if (shape instanceof Triangle) {
+                Triangle triangle = (Triangle) shape;
+                this.paintTriangle(triangle, g);
+            }
         }
     }
 }
